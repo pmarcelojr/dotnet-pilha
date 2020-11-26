4d4548011b060ab769420840e0c5e4c19bae75f9
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-11-26 15:36:08 -0300

    including code run program

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace dotnet_pilha
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var pilha = new Pilha();
+            pilha.Empilha(1);
+            pilha.Empilha(10);
+            pilha.Empilha(100);
+            Console.WriteLine(pilha.Desempilha());
+            Console.WriteLine(pilha.Desempilha());
+            Console.WriteLine(pilha.Desempilha());
         }
     }
 }
